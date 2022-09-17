b1f369a141f6707eeca1aa8fadb12dae80382d68
alexovna <alexovna.maker@gmail.com>
2022-09-16 19:54:00 -0700

    task 1/2 for review

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,3 +1,40 @@
+import collections
+
+students = [
+    {'first_name': 'Вася'},
+    {'first_name': 'Петя'},
+    {'first_name': 'Маша'},
+    {'first_name': 'Маша'},
+    {'first_name': 'Петя'},
+]
+
+
+# --- main function ---
+
+def count_frequency_name():
+    name_students = []
+    key = 'first_name'
+
+    for i in range(len(students)):
+        name_students.append(students[i][key])
+        frequency_name_students = collections.Counter(name_students).most_common()
+
+    for i in range(len(frequency_name_students)):
+        name = frequency_name_students[i][0]
+        frequency = frequency_name_students[i][1]
+        print(f'{name} : {frequency}')
+
+    def print_format_high_frequency_item(sourse=frequency_name_students):
+        highest_frequency_name = f'\nHighest frequency student name: {sourse[0][0]}'
+        print(highest_frequency_name)
+
+    print_format_high_frequency_item()
+
+    return frequency_name_students
+
+# --- main function ---
+
+
 # Задание 1
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика
 # Пример вывода:
@@ -5,28 +42,19 @@
 # Маша: 2
 # Петя: 2
 
-students = [
-    {'first_name': 'Вася'},
-    {'first_name': 'Петя'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Петя'},
-]
-# ???
 
 
 # Задание 2
 # Дан список учеников, нужно вывести самое часто повторящееся имя
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
-students = [
-    {'first_name': 'Вася'},
-    {'first_name': 'Петя'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Маша'},
-    {'first_name': 'Оля'},
-]
-# ???
+# students = [
+#     {'first_name': 'Вася'},
+#     {'first_name': 'Петя'},
+#     {'first_name': 'Маша'},
+#     {'first_name': 'Маша'},
+#     {'first_name': 'Оля'},
+# ]
 
 
 # Задание 3
@@ -34,6 +62,7 @@ students = [
 # Пример вывода:
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
+
 
 school_students = [
     [  # это – первый класс
@@ -51,13 +80,40 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
+
+
+def count_frequency_name_among_classes():
+    name_students_in_class = []
+    key = 'first_name'
+
+    for n in range(len(school_students)):
+        name_students_in_class.append(school_students[n])
+    # print(name_students_in_class)
+    frequency_name_students = collections.Counter(name_students_in_class).most_common()
+    print(frequency_name_students)
+
+    # for i in range(len(frequency_name_students)):
+    #     name = frequency_name_students[i][0]
+    #     frequency = frequency_name_students[i][1]
+    #     print(f'{name} : {frequency}')
+
+    # def print_format_high_frequency_item(sourse=frequency_name_students):
+    #     highest_frequency_name = f'\nHighest frequency student name: {sourse[0][0]}'
+    #     print(highest_frequency_name)
+
+    # print_format_high_frequency_item()
+
+    # return frequency_name_in_class
+
+
+count_frequency_name_among_classes()
+
 
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
 # Пример вывода:
-# Класс 2a: девочки 2, мальчики 0 
+# Класс 2a: девочки 2, мальчики 0
 # Класс 2б: девочки 0, мальчики 2
 
 school = [
@@ -93,3 +149,4 @@ is_male = {
 }
 # ???
 
+# count_frequency_name()
