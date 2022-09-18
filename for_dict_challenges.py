81b0ed50bfd161089e5ab25d81b59a1dbd7590ec
alexovna <alexovna.maker@gmail.com>
2022-09-18 14:19:27 -0700

    the programm working with err: first top name not added to the list of results <top_name_among_school_classes>

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -8,60 +8,32 @@ students = [
     {'first_name': 'Петя'},
 ]
 
-
-# --- main function ---
-
-def count_frequency_name():
-    name_students = []
-    key = 'first_name'
-
-    for i in range(len(students)):
-        name_students.append(students[i][key])
-        frequency_name_students = collections.Counter(name_students).most_common()
-
-    for i in range(len(frequency_name_students)):
-        name = frequency_name_students[i][0]
-        frequency = frequency_name_students[i][1]
-        print(f'{name} : {frequency}')
-
-    def print_format_high_frequency_item(sourse=frequency_name_students):
-        highest_frequency_name = f'\nHighest frequency student name: {sourse[0][0]}'
-        print(highest_frequency_name)
-
-    print_format_high_frequency_item()
-
-    return frequency_name_students
-
-# --- main function ---
+key = 'first_name'
+# name_students = []
+# frequency_name = []
 
 
-# Задание 1
-# Дан список учеников, нужно посчитать количество повторений каждого имени ученика
-# Пример вывода:
-# Вася: 1
-# Маша: 2
-# Петя: 2
+# счетчик кол-ва повторений имени в списке
+def count_frequency_name(data_list):
+    students_name = [student.get(key) for student in data_list]
+    frequency_name = collections.Counter(students_name).most_common()
+    for name in range(len(frequency_name)):
+        print(f'{frequency_name[name][0]}:{frequency_name[name][1]}')
+    return frequency_name
 
 
-
-# Задание 2
-# Дан список учеников, нужно вывести самое часто повторящееся имя
-# Пример вывода:
-# Самое частое имя среди учеников: Маша
-# students = [
-#     {'first_name': 'Вася'},
-#     {'first_name': 'Петя'},
-#     {'first_name': 'Маша'},
-#     {'first_name': 'Маша'},
-#     {'first_name': 'Оля'},
-# ]
+def get_higest_frequency_name(data_frequency_name):
+    higest_frequency_name = []
+    for value in range(1, len(data_frequency_name)):
+        if data_frequency_name[0][1] < data_frequency_name[value][1]:
+            higest_frequency_name = data_frequency_name[value][1]
+        else:
+            higest_frequency_name = data_frequency_name[0][0]
+    print(f'Highest frequency student name: {higest_frequency_name}\n')
+    return higest_frequency_name
 
 
-# Задание 3
-# Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
-# Пример вывода:
-# Самое частое имя в классе 1: Вася
-# Самое частое имя в классе 2: Маша
+# get_higest_frequency_name(data_frequency_name=count_frequency_name(data_list=students))
 
 
 school_students = [
@@ -73,7 +45,8 @@ school_students = [
         {'first_name': 'Маша'},
         {'first_name': 'Маша'},
         {'first_name': 'Оля'},
-    ],[  # это – третий класс
+    ],
+    [  # это – третий класс
         {'first_name': 'Женя'},
         {'first_name': 'Петя'},
         {'first_name': 'Женя'},
@@ -82,71 +55,24 @@ school_students = [
 ]
 
 
-def count_frequency_name_among_classes():
-    name_students_in_class = []
-    key = 'first_name'
-
-    for n in range(len(school_students)):
-        name_students_in_class.append(school_students[n])
-    # print(name_students_in_class)
-    frequency_name_students = collections.Counter(name_students_in_class).most_common()
-    print(frequency_name_students)
-
-    # for i in range(len(frequency_name_students)):
-    #     name = frequency_name_students[i][0]
-    #     frequency = frequency_name_students[i][1]
-    #     print(f'{name} : {frequency}')
-
-    # def print_format_high_frequency_item(sourse=frequency_name_students):
-    #     highest_frequency_name = f'\nHighest frequency student name: {sourse[0][0]}'
-    #     print(highest_frequency_name)
-
-    # print_format_high_frequency_item()
-
-    # return frequency_name_in_class
+top_name_among_school_classes = []
+school_students_class = []
 
 
-count_frequency_name_among_classes()
+# счетчик кол-ва повторений имени в списке
+def count_frequency_name_school_students(school_students_data):
+    for class_student in school_students_data:
+        school_students_class = class_student
+
+        # список высокочастотных имен в разрезе одного класса исх-го списка
+        top_name_among_school_class = count_frequency_name(data_list=school_students_class)
+
+        # список имен высокочастотных имен в разрезе классов исх-го списка
+        top_name_among_school_classes.append(get_higest_frequency_name(data_frequency_name=top_name_among_school_class))
+    print(f'higest name among top name: {top_name_among_school_classes}')
 
 
+count_frequency_name_school_students(school_students_data=school_students)
 
-# Задание 4
-# Для каждого класса нужно вывести количество девочек и мальчиков в нём.
-# Пример вывода:
-# Класс 2a: девочки 2, мальчики 0
-# Класс 2б: девочки 0, мальчики 2
-
-school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-    {'class': '2б', 'students': [{'first_name': 'Даша'}, {'first_name': 'Олег'}, {'first_name': 'Маша'}]},
-]
-is_male = {
-    'Олег': True,
-    'Маша': False,
-    'Оля': False,
-    'Миша': True,
-    'Даша': False,
-}
-# ???
-
-
-# Задание 5
-# По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков
-# Пример вывода:
-# Больше всего мальчиков в классе 3c
-# Больше всего девочек в классе 2a
-
-school = [
-    {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
-    {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
-]
-is_male = {
-    'Маша': False,
-    'Оля': False,
-    'Олег': True,
-    'Миша': True,
-}
-# ???
-
-# count_frequency_name()
+# the programm working with err:
+# first top name not added to the list of results <top_name_among_school_classes>
